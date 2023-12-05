e52dbebcdcc7797b882e14a7e9980faf555ec9fb
Thomas <thomasjohnso2020@my.fit.edu>
2023-12-05 14:30:04 -0500

    Test tree to read from left to right, and create a binary tree

---
+++ tree.hs
@@ -0,0 +1,23 @@
+-- Binary Tree of generic type that can either be empty, or can contain an element and two other trees
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+-- let myTree = foldl treeInsert EmptyTree [a]
+
+-- tree with just one node, 
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => Tree a -> a -> Tree a
+treeInsert EmptyTree x = singleton x
+treeInsert (Node a left right) x
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert left x) right 
+    | x > a = Node a left (treeInsert right x)
+
+
+treeElem :: (Ord a) => Tree a -> a -> Bool
+treeElem EmptyTree x = False
+treeElem (Node a left right) x
+   | x == a = True
+   | x < a = treeElem left x
+   | x > a = treeElem right x
