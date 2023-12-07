797fd50dbd7cfbff90bf0caa326bcacf074a6a5e
Thomas <thomasjohnso2020@my.fit.edu>
2023-12-07 16:29:52 -0500

    rose to regex done, makin good progress!

--- derive.hs
+++ derive.hs
@@ -9,7 +9,33 @@ data Regex = EmptySet
   | ZeroOrMore Regex
   | Or Regex Regex
   | OneOrMore Regex
-  | Optional Regex
+  | Optional Regex deriving (Show)
+
+data RoseTree a = Node a [RoseTree a] deriving (Show)
+-- Text expression "ab@b|"
+-- Should be (a@b)|b
+--        |
+--      /   \
+--     @     b
+--    / \ 
+--   a   b
+
+-- This structure works with how derive.hs already works
+-- let pattern = Or ()
+-- let myTree= Node '|' [Node '@' [Node 'a' [], Node 'b' []], Node 'b'[]]
+
+
+roseTreeToRegex :: RoseTree Char -> Regex
+roseTreeToRegex (Node '|' [left, right]) = Or (roseTreeToRegex left) (roseTreeToRegex right)
+roseTreeToRegex (Node '@' [left, right]) = Concat (roseTreeToRegex left) (roseTreeToRegex right)
+roseTreeToRegex (Node '*' [child]) = ZeroOrMore (roseTreeToRegex child)
+roseTreeToRegex (Node '+' [child]) = OneOrMore (roseTreeToRegex child)
+roseTreeToRegex (Node '?' [child]) = Optional (roseTreeToRegex child)
+roseTreeToRegex (Node 'ε' []) = EmptyString
+roseTreeToRegex (Node '∅' []) = EmptySet
+roseTreeToRegex (Node c []) = Character c
+roseTreeToRegex _ = EmptySet 
+
 
 nullable :: Regex -> Bool
 nullable EmptySet = False
@@ -17,9 +43,10 @@ nullable EmptyString = True
 nullable Character{} = False
 nullable (Concat a b) = nullable a && nullable b
 nullable ZeroOrMore{} = True
+nullable (Or a b) = nullable a || nullable b
+-- Need revising
 nullable (OneOrMore _) = False
 nullable (Optional _) = True
-nullable (Or a b) = nullable a || nullable b
 
 deriv :: Regex -> Char -> Regex
 deriv EmptyString _ = EmptySet
@@ -32,7 +59,8 @@ deriv (Concat r s) c = if nullable r
 deriv (ZeroOrMore r) c =
   deriv r c `Concat` ZeroOrMore r
 deriv (Or r s) c =
-  deriv r c `Or` deriv s c
+  deriv r c `Or` deriv s c 
+-- Need revising
 deriv (OneOrMore r) c =
   deriv r c `Concat` ZeroOrMore r
 deriv (Optional r) c = 
